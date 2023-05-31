1457b9aa1f46d9a54aaf8becd169660579a57105
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-05-31 07:23:39 +0700

    1105

---
+++ Java/src/exercise/inheritance/bookshop/GoldenEditionBook.java
@@ -0,0 +1,12 @@
+package exercise.inheritance.bookshop;
+
+public class GoldenEditionBook extends Book{
+    public GoldenEditionBook(String author, String title, double price) {
+        super(author, title, price);
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice() + super.getPrice() * 0.3;
+    }
+}
---
+++ Java/src/exercise/inheritance/mankind/Student.java
@@ -0,0 +1,27 @@
+package exercise.inheritance.mankind;
+
+public class Student extends Human{
+    private int facultyNumber;
+
+    public Student(String firstName, String lastName, int facultyNumber) {
+        super(firstName, lastName);
+        this.setFacultyNumber(facultyNumber);
+    }
+
+    public int getFacultyNumber() {
+        return facultyNumber;
+    }
+
+    public void setFacultyNumber(int facultyNumber) {
+        this.facultyNumber = facultyNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student {" +
+                "firstName='" + getFirstName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                ", facultyNumber='" + getFacultyNumber() + '\'' +
+                '}';
+    }
+}
