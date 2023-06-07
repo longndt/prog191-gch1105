abe2fef6e9a91261fec1eb759f040d284d49303d
Long <longndt226@gmail.com>
2023-06-07 10:41:33 +0700

    1105

---
+++ Java/src/lab10/FileReadDemo.java
@@ -0,0 +1,4 @@
+package lab10;
+
+public class FileReadDemo {
+}
---
+++ Java/src/lab10/PrintWriterDemo1.java
@@ -0,0 +1,34 @@
+package lab10;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class PrintWriterDemo1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        //1. get data from keyboard's input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter birth year: ");
+        int birthYear = scanner.nextInt();
+        scanner.nextLine();    //remove buffer's cache
+        System.out.print("Enter mobile: ");
+        String mobile = scanner.nextLine();
+
+        //1A. calculate age
+        int currentYear = LocalDate.now().getYear();
+        int age = currentYear - birthYear;
+
+        //2. save data to text file
+        String fileName = "student.txt";
+        PrintWriter writer = new PrintWriter(fileName);
+        writer.println("Name: " + name);
+        writer.println("Birth year: " + birthYear);
+        writer.println("Age: " + age);
+        writer.println("Mobile phone: " + mobile);
+        writer.close();
+        System.out.println("Save student data to file " + fileName + " successfully !");
+    }
+}
